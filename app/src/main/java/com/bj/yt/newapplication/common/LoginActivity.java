6675be3f3786d701ea30c9a7e65c14e9d27a95c1
wxixis <695973530@qq.com>
2017-03-01 15:37:36 +0800

    Merge branch 'master' of /Users/wxixis/AndroidStudioProjects/NewApplication with conflicts.

---
+++ app/src/main/java/com/bj/yt/newapplication/common/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.bj.yt.newapplication.common;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.bj.yt.newapplication.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
