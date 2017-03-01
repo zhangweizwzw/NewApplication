e32e25a4f2a7fabd3508c2926fe19f61e7eccd7d
zhangweizwzw <zhangwei_7154@163.com>
2017-03-01 14:09:43 +0800

    创建application

---
+++ app/src/main/java/com/bj/yt/newapplication/common/MyApplication.java
@@ -0,0 +1,15 @@
+package com.bj.yt.newapplication.common;
+
+import android.app.Application;
+
+/**
+ * Created by admin on 2017/3/1.
+ */
+
+public class MyApplication extends Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
